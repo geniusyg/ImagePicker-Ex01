2b09a2e92769096e4a481324bf1b37c571f33f7e
Chae Yungi <l8673901@gmail.com>
2014-01-06 14:34:13 +0900

    ImagePicker Ex01
    ImagePicker Ex01

--- ExImagePicker/ViewController.m
+++ ExImagePicker/ViewController.m
@@ -7,12 +7,41 @@
 //
 
 #import "ViewController.h"
+#define MAX_NUM 5
 
-@interface ViewController ()
+@interface ViewController () <UIPickerViewDataSource, UIPickerViewDelegate>
+@property (weak, nonatomic) IBOutlet UIPickerView *picker;
 
 @end
 
 @implementation ViewController
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+	return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+	return MAX_NUM;
+}
+
+- (CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component {
+	return 64;
+}
+
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view {
+	NSString *imagePath = [NSString stringWithFormat:@"sns%d.png",row];
+	UIImage *img = [UIImage imageNamed:imagePath];
+	
+	UIImageView *imageView;
+	if(nil == view) {
+		imageView = [[UIImageView alloc] initWithImage:img];
+		imageView.frame = CGRectMake(0, 0, 100, 60);
+	} else {
+		imageView = (UIImageView *)view;
+		imageView.image = img;
+	}
+	return imageView;	
+}
 
 - (void)viewDidLoad
 {
@@ -26,4 +55,9 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)selectRandom:(id)sender {
+	int r = arc4random() % MAX_NUM;
+	[self.picker selectRow:r inComponent:0 animated:YES];
+}
 @end
