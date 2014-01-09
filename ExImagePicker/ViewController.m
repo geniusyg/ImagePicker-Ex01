a2d4d1016a8f48d3a9fc991b2b03dc7e1a3a9bc4
Chae Yungi <l8673901@gmail.com>
2014-01-09 09:36:32 +0900

    SlotMachine02
    SlotMachine02

--- ExImagePicker/ViewController.m
+++ ExImagePicker/ViewController.m
@@ -9,15 +9,19 @@
 #import "ViewController.h"
 #define MAX_NUM 5
 
-@interface ViewController () <UIPickerViewDataSource, UIPickerViewDelegate>
+@interface ViewController () <UIPickerViewDataSource, UIPickerViewDelegate> {
+	NSInteger don;
+}
 @property (weak, nonatomic) IBOutlet UIPickerView *picker;
+@property (weak, nonatomic) IBOutlet UITextField *money;
+@property (weak, nonatomic) IBOutlet UILabel *lucky;
 
 @end
 
 @implementation ViewController
 
 - (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
-	return 1;
+	return 3;
 }
 
 - (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
@@ -57,7 +61,48 @@
 
 
 - (IBAction)selectRandom:(id)sender {
-	int r = arc4random() % MAX_NUM;
-	[self.picker selectRow:r inComponent:0 animated:YES];
+	NSNumberFormatter * f = [[NSNumberFormatter alloc] init];
+	[f setNumberStyle:NSNumberFormatterDecimalStyle];
+	NSNumber * myNumber = [f numberFromString:self.money.text];
+	don = [myNumber intValue];
+	
+	int r1 = arc4random() % MAX_NUM;
+	int r2 = arc4random() % MAX_NUM;
+	int r3 = arc4random() % MAX_NUM;
+	
+	if(r1==r2 || r1==r3 || r2==r3) {
+		self.lucky.text = [NSString stringWithFormat:@"%d", don*10];
+	} else if(r1==r2 && r2==r3) {
+		self.lucky.text = [NSString stringWithFormat:@"%d", don*100];
+	} else {
+		self.lucky.text = @"빵";
+	}
+	
+	
+	[self.picker selectRow:r1 inComponent:0 animated:YES];
+	[self.picker selectRow:r2 inComponent:1 animated:YES];
+	[self.picker selectRow:r3 inComponent:2 animated:YES];
 }
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
